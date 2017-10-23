6dcd297367c907aeb16cc452c6a0917699dc4652
aziemba <aleksandra.ziemba@praktykant.allindata.pl>
2017-10-23 20:42:58 +0200

    Add basic integration with fixer.io

--- src/main/java/com/github/xenteros/Application.java
+++ src/main/java/com/github/xenteros/Application.java
@@ -2,13 +2,18 @@ package com.github.xenteros;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
-@RestController
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
--- src/main/java/com/github/xenteros/controller/CurrencyExchangeController.java
+++ src/main/java/com/github/xenteros/controller/CurrencyExchangeController.java
@@ -1,16 +1,22 @@
 package com.github.xenteros.controller;
 
 import com.github.xenteros.dto.CurrencyExchangeResultDto;
+import com.github.xenteros.dto.FixerResponseDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("/api/currency-exchange")
 public class CurrencyExchangeController {
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @GetMapping("eur-to-pln/{eur}")
     public CurrencyExchangeResultDto eurToPln(@PathVariable String eur) {
@@ -19,13 +25,16 @@ public class CurrencyExchangeController {
         result.setFrom("EUR");
         result.setTo("PLN");
         BigDecimal fromValue = new BigDecimal(eur);
-        BigDecimal toValue = fromValue.multiply(new BigDecimal("4.223265"));
+
+        //pobieranie kursu euro do pln
+        FixerResponseDto response =
+restTemplate.getForEntity("http://api.fixer.io/latest?symbols=PLN", FixerResponseDto.class).getBody();
+
+        BigDecimal toValue = fromValue.multiply(new BigDecimal(response.getRates().get("PLN"))); //podstawiamy
 
         result.setFromValue(fromValue);
         result.setResult(toValue);
 
         return result;
-
     }
-
 }
