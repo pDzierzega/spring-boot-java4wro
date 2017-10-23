155949c01134bf7456d745a81972109f478330c7
aziemba <aleksandra.ziemba@praktykant.allindata.pl>
2017-10-23 20:01:38 +0200

    Add separate package for controllers and dtos
    * Added separate file for controller,
    * Created dedicated dto, to test marschaling.

--- src/main/java/com/github/xenteros/Application.java
+++ src/main/java/com/github/xenteros/Application.java
@@ -11,25 +11,4 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
-    @RequestMapping(method = RequestMethod.GET,
-                    value = "/hello-world")
-    public String helloWorld() {
-        return "Hello World";
-    }
-
-    @GetMapping("/hello-name/{name}")
-    public String helloName(@PathVariable String name) {
-        return "Hello " + name;
-    }
-
-    @GetMapping("/hello-param")
-    public String helloParam(@RequestParam(name = "name", required = false) String name) {
-        return "Hello " + name + " via param";
-    }
-
-    @PostMapping("/hello-body")
-    public String helloBody(@RequestBody String name) {
-        return "Hello " + name;
-    }
 }
---
+++ src/main/java/com/github/xenteros/controller/CurrencyExchangeController.java
@@ -0,0 +1,31 @@
+package com.github.xenteros.controller;
+
+import com.github.xenteros.dto.CurrencyExchangeResultDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping("/api/currency-exchange")
+public class CurrencyExchangeController {
+
+    @GetMapping("eur-to-pln/{eur}")
+    public CurrencyExchangeResultDto eurToPln(@PathVariable String eur) {
+
+        CurrencyExchangeResultDto result = new CurrencyExchangeResultDto();
+        result.setFrom("EUR");
+        result.setTo("PLN");
+        BigDecimal fromValue = new BigDecimal(eur);
+        BigDecimal toValue = fromValue.multiply(new BigDecimal("4.223265"));
+
+        result.setFromValue(fromValue);
+        result.setResult(toValue);
+
+        return result;
+
+    }
+
+}
