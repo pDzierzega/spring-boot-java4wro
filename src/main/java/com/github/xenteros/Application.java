7cce00f7ce303580971bbb0e5fe3a287102ceb8a
aziemba <aleksandra.ziemba@praktykant.allindata.pl>
2017-10-25 20:31:49 +0200

    Add Swagger documentation

--- src/main/java/com/github/xenteros/Application.java
+++ src/main/java/com/github/xenteros/Application.java
@@ -5,9 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableSwagger2
 public class Application {
 
     public static void main(String[] args) {
@@ -17,5 +25,26 @@ public class Application {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+    @Bean
+    public Docket newsApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("currencyExchamge")
+                .apiInfo(apiInfo())
+                .select()
+                .paths(regex("/api/.*"))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring REST Sample with Swagger")
+                .description("Spring REST Sample with Swagger")
+                .termsOfServiceUrl("http://www-03.ibm.com/software/sla/sladb.nsf/sla/bm?Open")
+                .contact("Niklas Heidloff")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://github.com/IBM-Bluemix/news-aggregator/blob/master/LICENSE")
+                .version("2.0")
+                .build();
     }
 }
