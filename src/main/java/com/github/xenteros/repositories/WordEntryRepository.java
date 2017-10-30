38979013f8fcfad50e02919603e7447651c3a7f0
xenteros <adam@gurgul.com.pl>
2017-10-30 18:49:36 +0100

    Prepare repository for another example project

--- src/main/java/com/github/xenteros/repositories/WordEntryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.github.xenteros.repositories;
-
-import com.github.xenteros.model.WordEntry;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WordEntryRepository extends JpaRepository<WordEntry, Long>{
-
-}
